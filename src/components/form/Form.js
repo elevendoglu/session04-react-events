7e1ad940af58786f2fffef9f87fa721efbf053e5
Ertugrul Levendoglu <11doglu@gmail.com>
2021-12-11 15:01:53 +0200

    form values applied in one function (name, email..)

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,31 +2,51 @@ import './styles.css';
 import { useState } from 'react';
 
 const Form = () => {
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [pass, setPass] = useState('');
-const [country, setCountry] = useState('');
+// const [name, setName] = useState('');
+// const [email, setEmail] = useState('');
+// const [pass, setPass] = useState('');
+// const [country, setCountry] = useState('');
+const [values, setValues] = useState({
+  name: '', 
+  email: '', 
+  pass: '', 
+  country: '',
+})
 
-const handleEmail = (e) => setEmail(e.target.value); 
-const handleName = (e) => setName(e.target.value); /* name target screning near "username: " */
-const handlePassword = (e) => setPass(e.target.value);
-const handleCountry = (e) => setCountry(e.target.value);
+// const handleEmail = (e) => setEmail(e.target.value); 
+// const handleName = (e) => setName(e.target.value); /* name target screning near "username: " */
+// const handlePassword = (e) => setPass(e.target.value);
+// const handleCountry = (e) => setCountry(e.target.value);
+const handleValues = (e) => {
+  setValues({...values, [e.target.name]: e.target.value});
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  alert(
-    `
-    userName: ${name}
-    email: ${email}
-    pass: ${pass}
-    country: ${country}
-    `
+  // alert(
+  //   `
+  //   userName: ${name}
+  //   email: ${email}
+  //   pass: ${pass}
+  //   country: ${country}
+  //   `
+
+  // );
+
+    alert(
+      `
+      userName: ${values.name}
+      email: ${values.email}
+      pass: ${values.pass}
+      country: ${values.country}
+      `
 
   );
-  setName("");
-  setEmail("");
-  setPass("");
-  setCountry("");
+
+  // setName("");
+  // setEmail("");
+  // setPass("");
+  // setCountry("");
 };
 
 // const handleName = (e) => console.log(e.target.value);     //target displaying on console screen
@@ -38,40 +58,74 @@ const handleSubmit = (e) => {
         <div>
           <p></p>
           <label>
-            <strong>Username: {name}</strong>
+            <strong>
+              {/* Username: {name} */}
+              Username: {values.name}
+              </strong>
           </label>
           <br />
           <input 
-          type="text" 
+          type="text"
+          name="name" 
           placeholder="name" 
-          value={name}
+          // value={name}
+          value={values.name}
           // defaultValue="Please enter your name" 
-          onChange={handleName} 
+          // onChange={handleName} 
+          onChange={handleValues} 
           />
         </div>
         <div>
           <p></p>
           <label>
-            <strong>E-mail: {email}</strong>
+            <strong>
+              {/* E-mail: {email} */}
+              E-mail: {values.email}
+              </strong>
           </label>
           <br />
-          <input type="email" placeholder="email" value={email} onChange={handleEmail}/>
+          <input type="email" 
+          name="email"
+          placeholder="email" 
+          value={values.email} 
+          // value={email} 
+          // onChange={handleEmail}
+          onChange={handleValues} 
+          />      
         </div>
         <div>
           <p></p>
           <label>
-            <strong>Password: {pass} </strong>
+            <strong>
+              {/* Password: {pass}  */}
+              Password: {values.pass} 
+              </strong>
           </label>
           <br />
-          <input type="password" placeholder="pass" value={pass} onChange={handlePassword} />
+          <input type="password" 
+          name="pass"
+          placeholder="pass" 
+          value={values.pass} 
+          // value={pass} 
+          // onChange={handlePassword} 
+          onChange={handleValues} 
+          />
         </div>
         <div style={{ margin: '10px auto' }}>
           <p></p>
           <label>
-            <strong>Country: {country} </strong>
+            <strong>
+              {/* Country: {country}  */}
+              Country: {values.country} 
+              </strong>
           </label>
-          <select onChange={handleCountry}
-            style={{ marginTop: '10px', width: '100px', padding: '10px' }}
+          <select 
+          // onChange={handleCountry} 
+          onChange={handleValues} 
+          name="country"
+          // value={country}
+          value={values.country}
+          style={{ marginTop: '10px', width: '100px', padding: '10px' }}
           >
             <option value="">Country</option>
             <option value="Turkey">Turkey</option>
@@ -86,6 +140,6 @@ const handleSubmit = (e) => {
       </form>
     </div>
   );
-};
+};  
 
 export default Form;
